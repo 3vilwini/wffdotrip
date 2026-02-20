051717242de1886f9b3b15654bdae95a4c1f5de0
tdingsun <tiger.dingsun@gmail.com>
2026-02-20 16:44:48 -0500

    styling

--- src/lib/components/index/CurrentResidentBanner.svelte
+++ src/lib/components/index/CurrentResidentBanner.svelte
@@ -1,6 +1,6 @@
-<div class="fixed right-0 top-0 w-14 h-[calc(100dvh-3.5rem)] z-1 shrink-0 overflow-hidden border-l bg-white">
+<div class="fixed right-0 top-0 w-14 h-[calc(100dvh-55px)] z-1 shrink-0 border-b overflow-hidden border-l bg-white">
 	<div
-		class="flex h-14 w-[calc(100dvh-3.5rem)] origin-top-left translate-x-14 rotate-90 items-center justify-center whitespace-nowrap"
+		class="flex h-14 w-[calc(100dvh-55px)] origin-top-left translate-x-14 rotate-90 items-center justify-center whitespace-nowrap"
 	>
 		Sophia Giovanniti
 	</div>
