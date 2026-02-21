43b33bf64d429a6e9640540e40ca9402a55c051c
tdingsun <tiger.dingsun@gmail.com>
2026-02-21 15:52:18 -0500

    styling tweak

--- src/routes/cost/+page.svelte
+++ src/routes/cost/+page.svelte
@@ -2,31 +2,23 @@
 	import logo3 from '$lib/assets/logo3.png';
 	import { siteState } from '$lib/states.svelte';
 	import BlockContent from '$lib/components/blockContent/BlockContent.svelte';
+	import MobileIndexHeader from '$lib/components/index/MobileIndexHeader.svelte';
 
 	let { data } = $props();
 </script>
 
-<div class="flex h-14 w-full items-center border-b px-4">
-	<div class="flex w-full flex-row items-center justify-between">
-		<a href="/" class="cursor-pointer">
-			<img src={logo3} class="w-60 cursor-pointer" />
-		</a>
-		<div class="flex items-center text-center font-mono text-xs">
-			How much does this website cost?
-		</div>
-		<div class="w-60"></div>
-	</div>
+<MobileIndexHeader></MobileIndexHeader>
+<div class="flex justify-center items-center px-3 pt-8 font-mono text-xs sm:hidden">
+	How much does this website cost?
 </div>
-
-<div class="mx-auto flex w-full max-w-[1500px] flex-col gap-16 p-3 sm:p-16">
-	
+<div class="mx-auto flex w-full max-w-[1500px] flex-col gap-16 p-3 pt-16 sm:p-16">
 	<div class="flex flex-wrap justify-evenly gap-4 sm:gap-16">
 		{#each data.costPage.sectionOne as item, idx}
 			<div class="flex basis-1/4 flex-col items-center">
 				<div
 					class="{idx % 2 === 0
 						? 'font-sans text-[22px] '
-						: 'font-serif text-2xl'} leading-[1.2] mb-2 text-center tracking-[-0.01em]"
+						: 'font-serif text-2xl'} mb-2 text-center leading-[1.2] tracking-[-0.01em]"
 				>
 					{item.title[siteState.language]}
 				</div>
@@ -40,10 +32,10 @@
 	<div class=" mx-auto max-w-3xl text-center font-mono text-xs">
 		<BlockContent value={data.costPage.sectionTwo[siteState.language]}></BlockContent>
 	</div>
-	
+
 	<div class="flex flex-wrap justify-between gap-16 sm:gap-0">
 		{#each data.costPage.sectionThree as item, idx}
-			<div class="flex sm:basis-1/5 flex-col items-center">
+			<div class="flex flex-col items-center sm:basis-1/5">
 				<div
 					class="{idx % 2 === 0
 						? 'font-sans text-[22px]'
