50984b83093ecc4fc1151f119cc26be08b926165
3vilwini <renxchristiane@gmail.com>
2026-05-03 22:42:46 +0200

    Increase button text size for SubmitEntryButton

--- src/lib/components/index/SubmitEntryButton.svelte
+++ src/lib/components/index/SubmitEntryButton.svelte
@@ -9,7 +9,7 @@
 <a
   href="/form"
   class="fixed bottom-3 left-[calc(50dvw)] translate-x-[-50%] cursor-pointer px-3 py-2 sm:px-6 sm:py-3 uppercase sm:bottom-13.5
-  bg-black text-white outline outline-dashed outline-black
+  bg-black text-white text-2xl outline outline-dashed outline-black
   hover:bg-white hover:text-black"
 >
   {fieldLabels.submitAnEntry[siteState.language]}
