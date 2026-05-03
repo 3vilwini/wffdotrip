80e6951b2caddba0a3473151e44432da01ecd9c9
3vilwini <renxchristiane@gmail.com>
2026-05-03 22:41:44 +0200

    Update SubmitEntryButton.svelte

--- src/lib/components/index/SubmitEntryButton.svelte
+++ src/lib/components/index/SubmitEntryButton.svelte
@@ -7,10 +7,10 @@
 
 
 <a
-	href="/form"
-	class="fixed bottom-3 left-[calc(50dvw)] translate-x-[-50%] cursor-pointer px-3 py-2 sm:px-6 sm:py-3 uppercase sm:bottom-13.5
-	bg-black text-white border border-transparent
-	hover:bg-white hover:text-black hover:border-black hover:border-dashed"
+  href="/form"
+  class="fixed bottom-3 left-[calc(50dvw)] translate-x-[-50%] cursor-pointer px-3 py-2 sm:px-6 sm:py-3 uppercase sm:bottom-13.5
+  bg-black text-white outline outline-dashed outline-black
+  hover:bg-white hover:text-black"
 >
-	{fieldLabels.submitAnEntry[siteState.language]}
+  {fieldLabels.submitAnEntry[siteState.language]}
 </a>
