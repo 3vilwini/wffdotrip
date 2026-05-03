79e71c17e5c00e8a78325b03d4cbfb157dc0c3ff
3vilwini <renxchristiane@gmail.com>
2026-05-03 19:26:55 +0200

    Update SubmitEntryButton styles for hover effect

--- src/lib/components/index/SubmitEntryButton.svelte
+++ src/lib/components/index/SubmitEntryButton.svelte
@@ -4,9 +4,13 @@
 	import { siteState } from '$lib/states.svelte';
 </script>
 
+
+
 <a
 	href="/form"
-	class="fixed bottom-3 left-[calc(50dvw)] translate-x-[-50%] cursor-pointer border border-dashed bg-black px-3 py-2 sm:px-6 sm:py-3 text-white uppercase sm:bottom-13.5"
+	class="fixed bottom-3 left-[calc(50dvw)] translate-x-[-50%] cursor-pointer px-3 py-2 sm:px-6 sm:py-3 uppercase sm:bottom-13.5
+	bg-black text-white border border-transparent
+	hover:bg-white hover:text-black hover:border-black hover:border-dashed"
 >
 	{fieldLabels.submitAnEntry[siteState.language]}
 </a>
