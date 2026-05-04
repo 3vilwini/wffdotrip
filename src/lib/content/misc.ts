f3d9dd7a451887eff97d3969e166b9c082a3982c
3vilwini <renxchristiane@gmail.com>
2026-05-04 23:05:47 +0200

    Update Dutch translations in misc.ts

--- src/lib/content/misc.ts
+++ src/lib/content/misc.ts
@@ -5,25 +5,25 @@ export const indexHeaderLabels = {
 		[Language.EN]: 'Worker',
 		[Language.FR]: 'Travailleur·se',
 		[Language.IT]: 'Lavoratorə',
-		[Language.NL]: 'Worker (NL)'
+		[Language.NL]: 'Werknemer'
 	},
 	employer: {
 		[Language.EN]: 'Employer',
 		[Language.FR]: 'Employeur',
 		[Language.IT]: 'Datorə di lavoro',
-		[Language.NL]: 'Employer (NL)'
+		[Language.NL]: 'Werkgever'
 	},
 	contract: {
 		[Language.EN]: 'Contract',
 		[Language.FR]: 'Contrat',
 		[Language.IT]: 'Contratto',
-		[Language.NL]: 'Contract (NL)'
+		[Language.NL]: 'Contract'
 	},
 	wage: {
 		[Language.EN]: 'Wage',
 		[Language.FR]: 'Paie',
 		[Language.IT]: 'Retribuzione',
-		[Language.NL]: 'Wage (NL)'
+		[Language.NL]: 'Loon'
 	}
 };
 
@@ -32,19 +32,19 @@ export const navMenuItems = {
 		[Language.EN]: 'FAQ',
 		[Language.FR]: 'FAQ',
 		[Language.IT]: 'FAQ',
-		[Language.NL]: 'FAQ (NL)'
+		[Language.NL]: 'FAQ'
 	},
 	privacy: {
 		[Language.EN]: 'Privacy & data',
 		[Language.FR]: 'Confidentialité & données',
 		[Language.IT]: 'Privacy e dati',
-		[Language.NL]: 'Privacy & data (NL)'
+		[Language.NL]: 'Privacy & gegevens'
 	},
 	cost: {
 		[Language.EN]: 'How much does this website cost?',
 		[Language.FR]: 'Combien coûte cette plateforme ?',
 		[Language.IT]: 'Quanto costa questo sito web?',
-		[Language.NL]: 'How much does this website cost? (NL)'
+		[Language.NL]: 'Wat kost deze website?'
 	}
 };
 
@@ -53,47 +53,47 @@ export const fieldLabels = {
 		[Language.EN]: 'Country',
 		[Language.FR]: 'Pays',
 		[Language.IT]: 'Paese',
-		[Language.NL]: 'Country (NL)'
+		[Language.NL]: 'Land'
 	},
 	city: {
 		[Language.EN]: 'City (optional)',
 		[Language.FR]: 'Ville (facultatif)',
 		[Language.IT]: 'Città (facoltativo)',
-		[Language.NL]: 'City (optional) (NL)'
+		[Language.NL]: 'Stad (optioneel)'
 	},
 	year: {
 		[Language.EN]: 'Year',
 		[Language.FR]: 'Année',
 		[Language.IT]: 'Anno',
-		[Language.NL]: 'Year (NL)'
+		[Language.NL]: 'Jaar (optioneel)'
 	},
 	employerType: {
-		[Language.EN]: 'Employer Category',
+		[Language.EN]: 'Employer category',
 		[Language.FR]: 'Catégorie d’employeur',
 		[Language.IT]: 'Tipologia di datore di lavoro',
-		[Language.NL]: 'Employer Category (NL)'
+		[Language.NL]: 'Werkgever type'
 	},
 	employerName: {
-		[Language.EN]: 'Employer Name (optional)',
+		[Language.EN]: 'Employer name (optional)',
 		[Language.FR]: 'Nom de l’employeur (facultatif)',
 		[Language.IT]: 'Nome del datore di lavoro (facoltativo)',
-		[Language.NL]: 'Employer Name (optional) (NL)'
+		[Language.NL]: 'Naam werkgever (optioneel)'
 	},
 	numEmployees: {
-		[Language.EN]: '# of Employees',
+		[Language.EN]: '# of employees',
 		[Language.FR]: '# d’employé·es',
 		[Language.IT]: '# di dipendentə',
-		[Language.NL]: '# of Employees (NL)'
+		[Language.NL]: '# aantal werknemers'
 	},
 	contractType: {
-		[Language.EN]: 'Contract Type',
+		[Language.EN]: 'Contract type',
 		[Language.FR]: 'Type de contrat',
 		[Language.IT]: 'Tipologia di contratto',
-		[Language.NL]: 'Contract Type (NL)'
+		[Language.NL]: 'Contract type'
 	},
 	workerStatus: {
 		[Language.EN]: {
-			[Country.UK]: 'Worker Status (UK)',
+			[Country.UK]: 'Worker status (UK)',
 			[Country.NETHERLANDS]: 'Worker Status (NL)',
 			[Country.ITALY]: 'Worker Status (IT)',
 			[Country.FRANCE]: 'Worker Status (FR)'
@@ -111,131 +111,131 @@ export const fieldLabels = {
 			[Country.FRANCE]: 'Status del lavoratorɜ (FR)'
 		},
 		[Language.NL]: {
-			[Country.UK]: 'UK Worker status (NL)',
-			[Country.NETHERLANDS]: 'Dutch Worker status (NL)',
-			[Country.ITALY]: 'Italian Worker status (NL)',
-			[Country.FRANCE]: 'French Worker status (NL)'
+			[Country.UK]: 'Werknemer status (UK)',
+			[Country.NETHERLANDS]: ' Werknemer status (NL)',
+			[Country.ITALY]: 'Werknemer status (IT)',
+			[Country.FRANCE]: 'Werknemer status (FR)'
 		}
 	},
 	contractLength: {
-		[Language.EN]: 'Contract Length (optional)',
+		[Language.EN]: 'Contract length (optional)',
 		[Language.FR]: 'Durée du contrat (facultatif)',
 		[Language.IT]: 'Durata del contratto (facoltativo)',
-		[Language.NL]: 'Contract Length (optional) (NL)'
+		[Language.NL]: 'Contract duur (optioneel)'
 	},
 	contractNumHours: {
-		[Language.EN]: '# of Hours (optional)',
+		[Language.EN]: '# of hours (optional)',
 		[Language.FR]: '# d’heures (facultatif)',
 		[Language.IT]: '# di ore (facoltativo)',
-		[Language.NL]: '# of Hours (optional) (NL)'
+		[Language.NL]: '# uren (optioneel)'
 	},
 	perWeek: {
 		[Language.EN]: 'hours / week',
 		[Language.FR]: 'heures / semaine',
 		[Language.IT]: 'ore / settimana',
-		[Language.NL]: 'hours / week (NL)'
+		[Language.NL]: 'uren / week '
 	},
 	workerType: {
-		[Language.EN]: 'Worker Category',
+		[Language.EN]: 'Worker category',
 		[Language.FR]: 'Catégorie de travailleur·se',
 		[Language.IT]: 'Categoria del lavoratorə',
-		[Language.NL]: 'Worker Category (NL)'
+		[Language.NL]: 'Werknemer categorie'
 	},
 	projectType: {
-		[Language.EN]: 'Project Category',
-		[Language.FR]: 'Type de projet',
-		[Language.IT]: 'Tipologia di progetto',
-		[Language.NL]: 'Project Category (NL)'
-	},
-	jobTitle: {
-		[Language.EN]: 'Project Category (optional)',
+		[Language.EN]: 'Project type (optional)',
 		[Language.FR]: 'Type de projet (facultatif)',
 		[Language.IT]: 'Tipologia di progetto (facoltativo)',
-		[Language.NL]: 'Job title (optional) (NL)'
+		[Language.NL]: 'Project type (optioneel)'
+	},
+	jobTitle: {
+		[Language.EN]: 'Job title (optional)',
+		[Language.FR]: 'Intitulé de poste (facultatif)',
+		[Language.IT]: 'Titolo di lavoro (facoltativo)',
+		[Language.NL]: 'Functietitel (optioneel)'
 	},
 	jobDetails: {
 		[Language.EN]: 'Details (optional)',
 		[Language.FR]: 'Détails (facultatif)',
 		[Language.IT]: 'Dettagli (facoltativo)',
-		[Language.NL]: 'Details (optional) (NL)'
+		[Language.NL]: 'Detail (optioneel)'
 	},
 	jobExperience: {
 		[Language.EN]: 'Experience (optional)',
 		[Language.FR]: 'Expérience (facultatif)',
 		[Language.IT]: 'Esperienza (facoltativo)',
-		[Language.NL]: 'Experience (optional) (NL)'
+		[Language.NL]: 'Ervaring (optioneel)'
 	},
 	jobObtainedVia: {
-		[Language.EN]: 'Job Obtained Via (optional)',
+		[Language.EN]: 'Job obtained via (optional)',
 		[Language.FR]: 'Travail obtenu via (facultatif)',
 		[Language.IT]: 'Modalità di ottenimento del lavoro (facoltativo)',
-		[Language.NL]: 'Job obtained via (optional) (NL)'
+		[Language.NL]: 'Baan verkregen via (optioneel)'
 	},
 	compensationAmount: {
 		[Language.EN]: 'Amount',
 		[Language.FR]: 'Montant',
 		[Language.IT]: 'Importo',
-		[Language.NL]: 'Amount (NL)'
+		[Language.NL]: 'Bedrag'
 	},
 	compensationPercentage: {
 		[Language.EN]: 'Percentage',
 		[Language.FR]: 'Pourcentage',
 		[Language.IT]: 'Percentuale',
-		[Language.NL]: 'Percentage (NL)'
+		[Language.NL]: 'Percentage'
 	},
 	compensationFrequency: {
 		[Language.EN]: 'frequency',
 		[Language.FR]: 'fréquence',
 		[Language.IT]: 'frequenza',
-		[Language.NL]: 'frequency (NL)'
+		[Language.NL]: 'frequentie'
 	},
 	compensationLate: {
 		[Language.EN]: 'I was paid late',
 		[Language.FR]: 'J’ai été payé·e en retard',
 		[Language.IT]: 'Sono statə pagatə in ritardo',
-		[Language.NL]: 'I was paid late (NL)'
+		[Language.NL]: 'Ik werd te laat betaald'
 	},
 	satisfiedWithCompensation: {
 		[Language.EN]: 'I was satisfied with the compensation',
 		[Language.FR]: 'La rémunération m’est apparue satisfaisante',
 		[Language.IT]: 'Ero soddisfattə del compenso',
-		[Language.NL]: 'I was satisfied with the compensation (NL)'
+		[Language.NL]: 'Ik was tevreden over de vergoeding'
 	},
 	satisfiedWithConditions: {
 		[Language.EN]: 'I was satisfied with the working conditions',
 		[Language.FR]: 'Les conditions de travail me sont apparues satisfaisantes',
 		[Language.IT]: 'Ero soddisfattə delle condizioni di lavoro',
-		[Language.NL]: 'I was satisfied with the working conditions (NL)'
+		[Language.NL]: 'Ik was tevreden over de werkomstandigheden'
 	},
 	treatedFairly: {
 		[Language.EN]: 'I felt fairly treated against my colleagues',
 		[Language.FR]: 'Je me suis senti·e traité·e de manière équitable par rapport à mes collègues',
 		[Language.IT]: 'Mi sono sentitə trattatə in modo equo rispetto ai miei colleghi',
-		[Language.NL]: 'I felt fairly treated against my colleagues (NL)'
+		[Language.NL]: 'Ik werd gelijkwaardig als mijn collega’s behandeld'
 	},
 	addlComp: {
-		[Language.EN]: 'Additional Compensation',
+		[Language.EN]: 'Additional compensation',
 		[Language.FR]: 'Compensations additionelles',
 		[Language.IT]: 'Compensi aggiuntivi',
-		[Language.NL]: 'Additional Compensation (NL)'
+		[Language.NL]: 'Extra vergoeding'
 	},
 	employees: {
 		[Language.EN]: 'employees',
 		[Language.FR]: 'employé·es',
 		[Language.IT]: 'dipendentə',
-		[Language.NL]: 'employees (NL)'
+		[Language.NL]: 'werknemers'
 	},
 	yearsOfExperience: {
-		[Language.EN]: 'year(s) of experience',
-		[Language.FR]: 'année(s) d’expérience',
+		[Language.EN]: 'years of experience',
+		[Language.FR]: 'années d’expérience',
 		[Language.IT]: 'anni di esperienza',
-		[Language.NL]: 'year(s) of experience (NL)'
+		[Language.NL]: 'jaren ervaring'
 	},
 	submitAnEntry: {
 		[Language.EN]: 'Submit an entry',
 		[Language.FR]: 'Partager une rémuneration',
 		[Language.IT]: 'Condividere una remunerazione',
-		[Language.NL]: 'Submit an entry (NL)'
+		[Language.NL]: 'Een inzending indienen'
 	}
 };
 
@@ -244,24 +244,24 @@ export const miscLabels = {
 		[Language.EN]: 'Are you sure you want to delete this entry?',
 		[Language.FR]: 'Êtes-vous sûr·e de vouloir supprimer cette entrée ?',
 		[Language.IT]: 'Sei sicurə di voler eliminare questa voce?',
-		[Language.NL]: 'Are you sure you want to delete this entry? (NL)'
+		[Language.NL]: 'Weet je zeker dat je deze invoer wilt verwijderen?'
 	},
 	delete: {
 		[Language.EN]: 'Delete',
 		[Language.FR]: 'Supprimer',
 		[Language.IT]: 'Elimina',
-		[Language.NL]: 'Delete (NL)'
+		[Language.NL]: 'Verwijderen'
 	},
 	cancel: {
 		[Language.EN]: 'Cancel',
 		[Language.FR]: 'Annuler',
 		[Language.IT]: 'Annulla',
-		[Language.NL]: 'Cancel (NL)'
+		[Language.NL]: 'Annuleren'
 	}
 };
 export const yearOption2010OrBefore = {
 	[Language.EN]: '2010 or before',
 	[Language.FR]: '2010 ou avant',
 	[Language.IT]: '2010 o anni precedenti',
-	[Language.NL]: '2010 or before (NL)'
+	[Language.NL]: '2010 of eerder'
 };
