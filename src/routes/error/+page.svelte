bb5e2fdfa4bb6fb1fbb5c1cd140a9a43ae780e1d
3vilwini <renxchristiane@gmail.com>
2026-05-11 16:14:00 +0200

    Modify error message in error page
    Updated error message to provide a specific retry time and a link for bug reporting.

--- src/routes/error/+page.svelte
+++ src/routes/error/+page.svelte
@@ -1,5 +1,5 @@
 <div>
-	There has been an error submitting the form. Please try again later or contact TK@TK.com
+	There has been an error submitting the form. Please try again in 1 minute. If the problem continues, you can anonymously submit a bug with a screenshot of the entry <a href="https://framaforms.org/workforfreerip-bug-report-1778160852" target="_blank">here</a>.
 	<div>
 		<a href="/form">Back to Form</a>
 	</div>
